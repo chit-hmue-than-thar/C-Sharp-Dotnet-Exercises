5a6f739fb17de70aa68e542ee08b1136562ea726
chit-hmue-than-thar <juliawilson757@gmail.com>
2026-06-29 10:48:45 +0630

    feat: add more logics

--- JuneIntake.ConsoleApp/Program.cs
+++ JuneIntake.ConsoleApp/Program.cs
@@ -4,5 +4,23 @@ Console.WriteLine("Hello, World!");
 //string message_key = Console.ReadKey().KeyChar.ToString();
 //Console.WriteLine("hello " + message_key);
 
+int num;
+decimal dec_num;
+string str;
+DateTime dt;
+
+
 string message = Console.ReadLine();
 Console.WriteLine("You entered: " + message);
+
+Console.WriteLine("Enter your number: ");
+string input = Console.ReadLine();
+int result = Convert.ToInt32(input);
+Console.WriteLine("Converted Int Type" + result);
+
+
+Console.WriteLine("Using writeline, Enter age: ");
+Console.ReadLine();
+Console.Write("Using write, Enter age: ");
+Console.ReadLine();
+
