aabda62282a4842becd4fd2b36cbb4c7bacacb2c
chit-hmue-than-thar <juliawilson757@gmail.com>
2026-06-29 10:21:10 +0630

    feat: add basic concepts

--- JuneIntake.ConsoleApp/Program.cs
+++ JuneIntake.ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//string message_key = Console.ReadKey().KeyChar.ToString();
+//Console.WriteLine("hello " + message_key);
+
+string message = Console.ReadLine();
+Console.WriteLine("You entered: " + message);
